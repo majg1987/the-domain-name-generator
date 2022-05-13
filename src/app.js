8e118ecab3347e2f5714fd4b220cd6772bc268ff
majg1987 <majg1987@hotmail.com>
2022-05-13 16:12:36 +0000

    the domain name generator finalizado v1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function mostrarLista() {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const extension = [".com", ".net", ".us", ".iu"];
+  let lista = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          lista +=
+            "<li class = 'list-group-item  bg-warning text-primary fw-bolder'>" +
+            pronoun[i] +
+            adj[j] +
+            noun[k] +
+            extension[l] +
+            "</li>";
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+        }
+      }
+    }
+  }
+  return lista;
+}
+document.querySelector("#listar").addEventListener("click", () => {
+  document.querySelector("#lista").innerHTML = mostrarLista();
+});
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("#cerrar").addEventListener("click", () => {
+  document.querySelector("#lista").innerHTML = "";
+});
